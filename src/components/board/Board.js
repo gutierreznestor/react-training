6937e76df061dc08262d3fb8b933978ce9eda6b8
GutierrezNestor <nggutierrez.45@gmail.com>
2020-05-18 16:06:31 -0300

    Class component

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -2,29 +2,37 @@ import React from 'react';
 import './Board.css';
 import Item from '../item/Item';
 
-const Board = () => {
-    return (<div className="board">
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-    </div>)
+class Board extends React.Component  {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (<div className="board">
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+        </div>)
+    }
+
 }
 
 export default Board;
--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,18 +1,33 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './Item.css';
 import ReactCardFlip from 'react-card-flip';
 
-const Item = () => {
-    const [isFlipped, setIsFlipped] = useState(false);
-    return (<div>
-        <ReactCardFlip isFlipped={isFlipped} >
-            <div className="item" onClick={() => setIsFlipped(!isFlipped)}>
-            </div>
-            <div className="icon" onClick={() => setIsFlipped(!isFlipped)}>
-                <i className="fas fa-headphones fa-5x"></i>
-            </div>
-        </ReactCardFlip>
-    </div>)
+class Item extends React.Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFlipped: false,
+            disabled: false
+        }
+    }
+
+    render() {
+        return (<div>
+            <ReactCardFlip isFlipped={this.state.isFlipped} >
+                <div className="item" onClick={() => this.setState({
+                    isFlipped: !this.state.isFlippedd,
+                    disabled: !this.state.isFlipped
+                    })}>
+                </div>
+                <div className="icon" onClick={() => this.setState({
+                    isFlipped: !this.state.isFlippedd,
+                    disabled: !this.state.isFlipped
+                    })}>
+                    <i className="fas fa-headphones fa-5x"></i>
+                </div>
+            </ReactCardFlip>
+        </div>)
+    }
 }
 
 export default Item;
