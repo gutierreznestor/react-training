578f033ae799f51738bf2cb6582781549d821e79
GutierrezNestor <nggutierrez.45@gmail.com>
2020-05-18 13:18:07 -0300

    Fontawesome library and React card flip.

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Item.css';
+import ReactCardFlip from 'react-card-flip';
 
 const Item = () => {
-
-    return (<div className="item"></div>)
+    const [isFlipped, setIsFlipped] = useState(false);
+    return (<div>
+        <ReactCardFlip isFlipped={isFlipped} >
+            <div className="item" onClick={() => setIsFlipped(!isFlipped)}>
+            </div>
+            <div className="icon" onClick={() => setIsFlipped(!isFlipped)}>
+                <i className="fas fa-headphones fa-5x"></i>
+            </div>
+        </ReactCardFlip>
+    </div>)
 }
 
 export default Item;
